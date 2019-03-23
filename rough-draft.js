5fa62ac1d00031ee0563278607fb4bcd6990c3d9
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-03-22 21:24:38 -0700

    rough draft works

---
+++ rough-draft.js
@@ -0,0 +1,143 @@
+var inquirer = require('inquirer');
+
+var myName = ('Vit').toLowerCase();
+
+var letterObjects = [];
+
+var guess;
+
+function initialRun() {
+    letterObjects = [];
+
+    // takes a string and returns object - converts phrases into 
+    function WordDestructor(wordToGuess) {
+        this.wordArray = Array.from(wordToGuess);
+    }
+
+    // takes a single letter and makes it an object
+    function LetterConstructor(letter) {
+        this.letter = letter;
+        this.boolean = false;
+    }
+
+    guess = new WordDestructor(myName);
+    // console.log('this, guess: ', this, guess);
+    // console.log('guess: ', guess);
+
+    for (var i = 0; i < guess.wordArray.length; i++) {
+        var oneLetterObject = new LetterConstructor(guess.wordArray[i]);
+        letterObjects.push(oneLetterObject);
+    }
+
+    // console.log(guess.wordArray.length);
+
+    // console.log('letterObjects: ', letterObjects);
+    // console.log('-------------------------------------');
+    // console.log('letterObjects: ', letterObjects[0]);
+
+}
+
+initialRun();
+
+
+
+// This code sets the boolean value of letterObjects and prints blanks for false boolean and visible characters for true
+
+var guessesLeft = 0;
+var guessCount = 0;
+var userGuesses = [''];
+var leftToGuess = 999;
+
+function evaluateAndPrint() {
+    var blanks = [''];
+    // set the number of allowed guesses => length of current phrase times 1.5
+    guessesLeft = Math.round((letterObjects.length * 1.5) - guessCount);
+    // set the number of letters to guess to current phrase character length 
+    leftToGuess = letterObjects.length;
+
+    for (var i = 0; i < letterObjects.length; i++) {
+        // compare all current user guesses to current guess-phrase, if it's a match, set letterObject to true
+        for (var k = 0; k < userGuesses.length; k++) {
+            if (userGuesses[k] === letterObjects[i].letter) {
+                letterObjects[i].boolean = true;
+            }
+        }
+        // evaluate all letterObjects and append to var 'blanks' the characters set to true, underscore + space for false, and double-space for spaces
+        if (letterObjects[i].boolean) {
+            blanks.push(letterObjects[i].letter + ' ');
+            // decrement guesses left to user
+            leftToGuess--;
+        } else if (letterObjects[i].letter === ' ') {
+            blanks.push('  ');
+            // decrement spaces from guesses left to user
+            leftToGuess--;
+        } else {
+            blanks.push('_ ');
+        }
+    }
+    // concatenate the 'blanks' array and print to screen
+
+    console.log('\n\n\n\n\n');
+    console.log('==============================================\n');
+    console.log((blanks).join(''));
+    console.log('\n');
+    // console.log(userGuesses.length);
+    console.log('guessesLeft: ', guessesLeft);
+    console.log('guessCount: ', guessCount);
+    console.log('leftToGuess: ', leftToGuess);
+
+}
+
+
+function runGuess() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'userGuess',
+        message: 'Guess my name..'
+    }]).then(function (userInput) {
+        userGuesses.push(userInput.userGuess)
+        console.log('userGuesses: ', userGuesses);
+
+        guessCount++;
+        console.log('guessCount: ', guessCount);
+
+        inquirerRun();
+    });
+}
+
+function tryAgain() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "tryAgain",
+        message: "Try another?",
+    }]).then(function (userInput) {
+        if (userInput.tryAgain) {
+            initialRun();
+            userGuesses = [''];
+            guessCount = 0;
+            leftToGuess = 999;
+            inquirerRun();
+        }
+    });
+}
+
+function youWon() {
+    console.log('=================================')
+    console.log('\nCongrats! You guessed correctly!\n')
+    console.log('=================================')
+
+    tryAgain();
+}
+
+function inquirerRun() {
+    if (leftToGuess === 1) {
+        youWon();
+    } else if (guessCount < Math.round((letterObjects.length * 1.5))) {
+        evaluateAndPrint();
+        runGuess();
+    } else {
+        tryAgain();
+    }
+}
+
+inquirerRun();
